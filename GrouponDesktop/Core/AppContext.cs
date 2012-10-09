b3d45d05986fda8c81034a6877d55978068511eb
Federico Schmidt <schmidt.federico@hotmail.com>
2012-10-08 23:45:51 -0300

    AppContext y DBManager. Separación en carpetas.
    Separado lo que es GUI de lo que es la lógica de la aplicación.

---
+++ GrouponDesktop/Core/AppContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace GrouponDesktop.Core
+{
+    public sealed class AppContext
+    {
+        private static Hashtable appContext = new Hashtable();
+
+        public AppContext()
+        {
+            this.createDBManager();
+        }
+
+        private void createDBManager()
+        {
+            DBManager m = new DBManager("user id=gd;" +
+                                       "password=gd2012;server=localhost\\SQLSERVER2008;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=GD2C2012; " +
+                                       "connection timeout=60");
+            appContext.Add(typeof(DBManager), m);
+        }
+
+        public static Object getObject(Type t)
+        {
+            return appContext[t];
+        }
+    }
+}
---
+++ GrouponDesktop/Core/DBManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace GrouponDesktop.Core
+{
+    class DBManager
+    {
+        SqlConnection sqlConnection = new SqlConnection();
+
+        public DBManager(String connectionString)
+        {
+            sqlConnection.ConnectionString = connectionString;
+        }
+
+        public SqlConnection getConnection() {
+            return this.sqlConnection;
+        }
+    }
+}
