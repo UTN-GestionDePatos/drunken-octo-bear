6659552c66c1ca969c25a839227cd4a8460e8d4a
Veronica Fabbro <vnfabbro@hotmail.com>
2012-11-23 15:27:22 -0300

    adapto abm proveedores

--- Funcionalidades/ABMProveedores.sql
+++ Funcionalidades/ABMProveedores.sql
@@ -1,30 +1,33 @@
 --CREAR PROVEEDOR
-CREATE PROCEDURE CrearProveedor(@user,@pass,@rol,@cuit,@razon,@mail,@tel,@rubro,@nombre,@calle,@altura,@piso,@dpto,@localidad,@cp) 
+CREATE PROCEDURE CrearProveedor(@user varchar(30),@pass varchar(30),@cuit bigint,@razon_social varchar(30),@mail varchar(30),
+@telefono bigint,@direccion varchar(100),@ciudad varchar(30),@nombre_contacto varchar(30)) 
 AS
 BEGIN
-	insert into Logins values(@user,dbo.SHA256(@pass),@rol, dbo.idEstado('Habilitado') ,0)
-	insert into Direcciones(calle,altura,piso,departamento,codigo_postal ) values(@calle,@altura,@piso,@dpto,@cp)
-	insert into Proveedores values(@user,@cuit,@razon,@mail,@tel, SCOPE_IDENTITY(),SCOPE_IDENTITY(),@rubro,@nombre)
+	insert into Usuarios values(@user,GESTION_DE_PATOS.SHA256(@pass),'Proveedor', GESTION_DE_PATOS.idEstado('Habilitado') ,0)
+	insert into Proveedores values(@user,@cuit,@razon_social,@mail,@telefono, @direccion,@ciudad,SCOPE_IDENTITY(),@nombre_contacto)
 END
 
+go
+
 --MODFICAR PROVEEDOR
-CREATE PROCEDURE ModificarProveedor(@user,@rol,@cuit,@razon,@mail,@tel,@rubro,@nombre,@calle,@altura,@piso,@dpto,@localidad,@cp)
+CREATE PROCEDURE ModificarProveedor(@user varchar(30),@cuit bigint,@razon_social varchar(30),@mail varchar(30),
+@telefono bigint,@direccion varchar(100),@ciudad varchar(30),@nombre_contacto varchar(30), @estado varchar(20))
 AS
 BEGIN
-	Update Proveedores set cuit=@cuit,razon_social=@razon,mail=@mail,telefono=@tel,rubro=@rubro,nombre_contacto=@nombre	
-	Update Direcciones set d.calle=@calle, d.altura=@altura, d.piso=@piso, d.departamento=@dpto, d.codigo_postal=@cp 
-						from Direcciones d join Proveedores c on d.id_direccion = c.id_direccion AND c.username = @user
-	Update Localidades set localidad=@localidad
+	Update Proveedores set cuit=@cuit,razon_social=@razon_social,mail=@mail,telefono=@telefono,direccion = @direccion,
+	ciudad = @ciudad, nombre_contacto=@nombre_contacto
+						where username = @user
 	IF (@estado='Habilitado')
 	BEGIN
-	Update Logins set estado= dbo.idEstado('Habilitado') where username =@username
+	Update Usuarios set estado= GESTION_DE_PATOS.idEstado('Habilitado') where username =@user
 	END
 END
+go
 
 --ELIMINAR PROVEEDOR
 
-CREATE PROCEDURE EliminarProveedor(@user)--es la misma funcion que cliente!!
+CREATE PROCEDURE EliminarProveedor(@user varchar(30))
 AS
 BEGIN
-	Update Logins set estado= dbo.idEstado('Deshabilitado') where username =@user
+	Update Usuarios set estado= dbo.idEstado('Deshabilitado') where username =@user
 END
