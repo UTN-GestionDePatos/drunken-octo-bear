0bdc643c9ce68f6dfaa62427b51059e19fefe65c
Esteban Sager <estebansager@gmail.com>
2012-11-24 21:54:03 -0300

    detalle boludo

--- Funcionalidades/FacturarProveedor.sql
+++ Funcionalidades/FacturarProveedor.sql
@@ -1,18 +1,18 @@
-CREATE PROCEDURE FacturarProveedor (@proveedor varchar(30), @fecha_desde datetime, @fecha_hasta datetime, @monto float,@ret int output)
+CREATE PROCEDURE GESTION_DE_PATOS.FacturarProveedor (@proveedor varchar(30), @fecha_desde datetime, @fecha_hasta datetime, @monto float,@ret int output)
 AS
 BEGIN
 /*
-	0: ok
+	idfactura: ok
 	1: se overlapean los intervalos --> cupones ya facturados
 */
-	if exists (select * from Facturas where proveedor = @proveedor and (fecha_desde between @fecha_desde and @fecha_hasta or fecha_hasta between @fecha_desde and @fecha_hasta))
+	if exists (select * from GESTION_DE_PATOS.Facturas where proveedor = @proveedor and (fecha_desde between @fecha_desde and @fecha_hasta or fecha_hasta between @fecha_desde and @fecha_hasta))
 		begin
 			set @ret = 1
 			return
 		end
 	
-	select @ret = MAX(id_factura)+1 from Facturas
-	insert into Facturas values(@ret, @proveedor, @monto, @fecha_desde, @fecha_hasta)
+	select @ret = MAX(id_factura)+1 from GESTION_DE_PATOS.Facturas
+	insert into GESTION_DE_PATOS.Facturas values(@ret, @proveedor, @monto, @fecha_desde, @fecha_hasta)
 	
 	return
 	
